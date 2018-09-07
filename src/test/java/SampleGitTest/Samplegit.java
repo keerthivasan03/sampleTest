bceffce5cfb977ce5f0071cc48327ccb70cbeb86
keerthivasan03 <rtrkeerthivasan@gmail.com>
2018-09-07 10:37:34 +0530

    first

---
+++ src/test/java/SampleGitTest/Samplegit.java
@@ -0,0 +1,27 @@
+package SampleGitTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Samplegit {
+	@Test
+	public void logintest() throws IOException{
+		WebDriver wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		File src=((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("C:\\Users\\prabhakar\\workspace\\New folder\\samplegit\\screenshot\\image.jpg"));
+	}
+
+
+
+	
+
+}
